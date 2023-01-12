b2ed3419fbfa3b001cc55a6ea793c2f87eb41470
avazbekowna <111364660+avazbekowna@users.noreply.github.com>
2023-01-12 16:38:48 +0600

    Course-vetka (#5)

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Course_hero from "./Course_hero/Course_hero";
 
 
 
 const Course = () => {
     return (
         <div>
+            <Course_hero/>
 
         </div>
     );
